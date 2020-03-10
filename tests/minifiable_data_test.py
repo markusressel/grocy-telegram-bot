44aa20580ec68f235cbd36a9ee47e253bfc77f19
Markus Ressel <mail@markusressel.de>
2020-03-10 04:42:44 +0100

    test fix enabled tests

--- tests/minifiable_data_test.py
+++ tests/minifiable_data_test.py
@@ -6,13 +6,13 @@ class MinifiableDataTest(TestBase):
 
     def test_minifiable_data(self):
         callback_data = ShoppingListItemButtonCallbackData(
-            product_id=123,
+            shopping_list_item_id=123,
             button_click_count=0,
             shopping_list_amount=3
         )
 
         minified = callback_data.minify()
-        self.assertEqual(minified, '{"c":"s","p":123,"b":0,"s":3}')
+        self.assertEqual(minified, '{"c":"s","s":123,"b":0,"sh":3}')
 
         parsed_callback_data = ShoppingListItemButtonCallbackData.parse(minified)
         self.assertEqual(list(vars(callback_data).values()), list(vars(parsed_callback_data).values()))

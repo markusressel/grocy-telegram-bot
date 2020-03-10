25dea72ba5dff1890cfc8d3fe9d4626c3392c154
Markus Ressel <mail@markusressel.de>
2020-03-10 05:22:46 +0100

    setup.py fix for github push

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 from setuptools import setup, find_packages
@@ -17,6 +18,11 @@ elif GIT_BRANCH == "beta":
 elif GIT_BRANCH == "dev":
     DEVELOPMENT_STATUS = "Development Status :: 3 - Alpha"
     VERSION_NAME = "%s-dev" % VERSION_NUMBER
+elif (os.environ.get("TRAVIS_BRANCH", None) == os.environ.get("TRAVIS_TAG", None) == "v{}".format(VERSION_NUMBER)) or \
+        "GITHUB_RELEASE" in os.environ:
+    # travis tagged release branch
+    DEVELOPMENT_STATUS = "Development Status :: 5 - Production/Stable"
+    VERSION_NAME = VERSION_NUMBER
 else:
     print("Unknown git branch, using pre-alpha as default")
     DEVELOPMENT_STATUS = "Development Status :: 2 - Pre-Alpha"
